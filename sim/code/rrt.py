4243166e98d1a327670ad82f5bb61608ccc0e4d3
Joshua <jwjibilian@gmail.com>
2017-10-29 23:28:23 -0400

    intial commit

---
+++ sim/code/rrt.py
@@ -0,0 +1,140 @@
+from time import sleep
+
+from cmap import *
+from gui import *
+from utils import *
+from random import *
+
+MAX_NODES = 20000
+
+################################################################################
+# NOTE:
+# Before you start, please familiarize yourself with class Node in utils.py
+# In this project, all nodes are Node object, each of which has its own
+# coordinate and parent if necessary. You could access its coordinate by node.x
+# or node[0] for the x coordinate, and node.y or node[1] for the y coordinate
+################################################################################
+
+def step_from_to(node0, node1, limit=75):
+    ############################################################################
+    # TODO: please enter your code below.
+    # 1. If distance between two nodes is less than limit, return node1
+    # 2. Otherwise, return a node in the direction from node0 to node1 whose
+    #    distance to node0 is limit. Recall that each iteration we can move
+    #    limit units at most
+    # 3. Hint: please consider using np.arctan2 function to get vector angle
+    # 4. Note: remember always return a Node object
+    distance = get_dist(node0, node1)
+    if ( distance <= limit):
+        return node1
+    #print("this is arctan")
+    #print (np.arctan2((node0.x, node1.x),(node0.y, node1.y)))
+    #vector math is easier....
+    newx = (((node1.x - node0.x)/distance)*limit) + node0.x
+    newy = (((node1.y - node0.y) / distance) * limit) + node0.y
+    # print("math check")
+    # print(distance)
+    # print(get_dist(node0,Node((newx,newy))))
+    # print(node0.x, " ", newx, " ", node1.x)
+    # print(node0.y, " ", newy, " ", node1.y)
+    node1 = Node((newx, newy))
+    return node1
+
+    ############################################################################
+
+
+def node_generator(cmap):
+    rand_node = Node([-1,-1])
+    ############################################################################
+    # TODO: please enter your code below.
+    # 1. Use CozMap width and height to get a uniformly distributed random node
+    # 2. Use CozMap.is_inbound and CozMap.is_inside_obstacles to determine the
+    #    legitimacy of the random node.
+    # 3. Note: remember always return a Node object
+    pass
+
+
+    map_width, map_height = cmap.get_size()
+    #print(map_width, map_height)
+
+    while not cmap.is_inbound(rand_node) and not cmap.is_inside_obstacles(rand_node):
+        rand_node = Node([randint(0, map_width),randint(0, map_height)])
+
+
+
+    #print(rand_node.x, " ", rand_node.y)
+    ############################################################################
+    return rand_node
+
+
+def RRT(cmap, start):
+    cmap.add_node(start)
+
+    map_width, map_height = cmap.get_size()
+
+    while (cmap.get_num_nodes() < MAX_NODES):
+        ########################################################################
+        # TODO: please enter your code below.
+        # 1. Use CozMap.get_random_valid_node() to get a random node. This
+        #    function will internally call the node_generator above
+        # 2. Get the nearest node to the random node from RRT
+        # 3. Limit the distance RRT can move
+        # 4. Add one path from nearest node to random node
+        #
+        rand_node = None
+        nearest_node = None
+        pass
+        rand_node = cmap.get_random_valid_node()
+
+        thenodes = cmap.get_nodes()
+        for n in thenodes:
+            #print(n.x," ", n.y)
+            if nearest_node is None:
+                nearest_node = n
+            if get_dist(rand_node, n) < get_dist(rand_node, nearest_node):
+                nearest_node = n
+        #print("nearest node")
+        #print(nearest_node.x, nearest_node.y)
+        theLimit = 75
+        rand_node = step_from_to(nearest_node, rand_node, limit = theLimit)
+        #print("hello")
+        ########################################################################
+        #print("hello")
+        sleep(0.01)
+        cmap.add_path(nearest_node, rand_node)
+        if cmap.is_solved():
+            break
+
+    if cmap.is_solution_valid():
+        print("A valid solution has been found :-) ")
+    else:
+        print("Please try again :-(")
+
+################################################################################
+#                     DO NOT MODIFY CODE BELOW                                 #
+################################################################################
+
+class RRTThread(threading.Thread):
+    """Thread to run cozmo code separate from main thread
+    """
+
+    def __init__(self):
+        threading.Thread.__init__(self, daemon=True)
+
+    def run(self):
+        while not stopevent.is_set():
+            RRT(cmap, cmap.get_start())
+            sleep(100)
+            cmap.reset()
+        stopevent.set()
+
+
+if __name__ == '__main__':
+    global grid, stopevent
+    stopevent = threading.Event()
+    cmap = CozMap("maps/map1.json", node_generator)
+    visualizer = Visualizer(cmap)
+    robot = RRTThread()
+    robot.start()
+    visualizer.start()
+    stopevent.set()
